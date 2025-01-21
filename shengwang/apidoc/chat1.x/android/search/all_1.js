735f2126a48c6054040d43fc8911513e557316d3
xuchengpu mac <1550540124@qq.com>
2025-01-21 16:18:34 +0800

    add android api reference of shengwang-1.3.2

---
+++ shengwang/apidoc/chat1.x/android/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['mainpage_0',['mainpage',['../md___users_xuchengpu__desktop__project_huanxin_publish_4_12_0_agora_emclient_android_mainpage.html',1,'']]],
+  ['multidevicelistener_1',['MultiDeviceListener',['../interfaceio_1_1agora_1_1_multi_device_listener.html',1,'io::agora']]]
+];
---
+++ shengwang/apidoc/chat1.x/android/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['即时通讯_20im_20api_20参考_0',['即时通讯 IM API 参考',['../index.html',1,'']]]
+];
